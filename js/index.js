b171c50ddbcb7ea4138a722307a023f6b8634874
DYGames <dygames@naver.com>
2023-02-17 01:22:39 +0900

    feat: 인생 영화와 앨범 자동 생성, 방명록, step2

---
+++ js/index.js
@@ -0,0 +1,143 @@
+movies = [{
+    "title": "12 몽키즈",
+    "img": "https://movie-phinf.pstatic.net/20111223_236/1324587168774v1rWh_JPEG/movie_image.jpg",
+    "desc": "서기 2035년 미래의 인류는 바이러스 감염으로 인해 99%가 멸망하고 소수의 생손자들은 지상에서의 생활을 포기한 채 지하 세계에서 생활하게 된다.",
+    "link": "https://movie.naver.com/movie/bi/mi/basic.naver?code=17758"
+},
+{
+    "title": "펄프 픽션",
+    "img": "https://movie-phinf.pstatic.net/20111221_33/1324474363727fxRgN_JPEG/movie_image.jpg",
+    "desc": "어느 식당에 두 남녀 건달(팀 로스, 아만다 플러머 분)이 손님을 상대로 강도행각을 벌이는 사건으로부터 영화는 시작된다.",
+    "link": "https://movie.naver.com/movie/bi/mi/basic.naver?code=16571"
+},
+{
+    "title": "트레인스포팅",
+    "img": "https://movie-phinf.pstatic.net/20111222_226/1324494144330DlQSl_JPEG/movie_image.jpg",
+    "desc": "마크 렌턴(이완 맥그리거 분)은 에딘버러의 거리에서 두 명의 상점 감시자들에게 쫓기고 있다. 자켓에 넣어둔 훔친 물건들이 바닥에 떨어진다.",
+    "link": "https://movie.naver.com/movie/bi/mi/basic.naver?code=17967"
+},
+{
+    "title": "박하사탕",
+    "img": "https://movie-phinf.pstatic.net/20180416_198/1523869200043wycgA_JPEG/movie_image.jpg",
+    "desc": `스무 살 첫사랑, 그 순수로 떠나는 시간여행
+    다시 시작하고 싶다! 다시 시작할 수 있다!`,
+    "link": "https://movie.naver.com/movie/bi/mi/basic.naver?code=27219"
+},
+{
+    "title": "노인을 위한 나라는 없다",
+    "img": "https://movie-phinf.pstatic.net/20180716_50/1531704983188SS89n_JPEG/movie_image.jpg",
+    "desc": `총격전이 벌어진 끔찍한 현장에서 르웰린 모스(조슈 브롤린)는
+    우연히 이백만 달러가 들어있는 가방을 손에 넣는다.
+    그러나 이 가방을 찾는 또 다른 이가 있었으니 바로 살인마 안톤 시거(하비에르 바르뎀).
+    그리고 이들의 뒤를 쫓는 보안관 벨(토미 리 존스)까지 합세하면서
+    누구도 피할 수 없는, 목숨을 건 추격전이 시작된다.`,
+    "link": "https://movie.naver.com/movie/bi/mi/basic.naver?code=66751"
+}]
+
+albums = [{
+    "title": "Smashing Pumpkins - Mellon Collie And The Infinite Sadness",
+    "img": "https://lh3.googleusercontent.com/VMWBpSXgG5fcJ4bGGRpbrZm8YaSq33FM_k35KQ9da9vu-PZAxHdnvRL2vIFFoqAyucQ0IlHFHrBAwFhk=w544-h544-l90-rj",
+    "desc": "1995년 10월 23일 발매된 스매싱 펌킨스의 3집 앨범. 스매싱 펌킨스를 대표하는 곡들인 Bullet with Butterfly Wings, 1979, Tonight, Tonight 등이 포함되어있다. 28곡이 수록된 더블 앨범으로 발매 첫 주 빌보드 200차트 1위를 거머쥐었다. 발매 되자마자 대중과 평단의 극찬을 받아 천만장 이상 판매하여 다이아몬드 레코드 인증을 받았고 1997년 그래미 시상식에 7개 부문에 노미네이트 되었다.",
+    "link": "https://music.youtube.com/playlist?list=OLAK5uy_lvoCnzfuW1DyRCltvbMBxfUoKZGlBvKNs"
+},
+{
+    "title": "Pixies - Doolittle",
+    "img": "https://lh3.googleusercontent.com/tCnRO-5paB2Na8WhxMfV2q1dM7lLwChFgk19qgNPBPSEfOCWmfGHTvkgx2UABcxBSy-bxELjZ72-lPM=w544-h544-s-l90-rj",
+    "desc": "미국의 얼터너티브 밴드 픽시즈의 2번째 앨범. 1989년 4월 17일에 발매되었다. 평론가들에게 호평을 받았으며, 전작보다 더 다듬어지고 멜로디컬한 면이 두드러져 얼터너티브/인디 장르의 명반으로 자리잡았다. 길 노튼이 프로듀서로 참여하면서 3주간 앨범 제작이 이루어졌다. 전작만큼이나 가사의 내용이 괴상망측하다. 자살, 환경오염, 점성술, 성경 이야기 등 온갖 이야기가 섞인 가사를 자랑하는데, 보컬 블랙 프랜시스는 딱히 특별한 의미는 없고 곡에 어울리게 쓴 것이라고 했다. 이런 초현실적인 이미지는 데이비드 린치의 인터뷰에 영감을 받았다고 한다.",
+    "link": "https://music.youtube.com/playlist?list=OLAK5uy_lf6jsYkBOcfA6eAzNNIr2PeQv0Cns519A"
+},
+{
+    "title": "세이수미 - The Last Thing Left",
+    "img": "https://lh3.googleusercontent.com/XF6jRih4HSh3H0gFL4keLO1GurCD1BqcGayZjsTKk0R_Ijcpx3vJBu1oU8DJ2vin7GSVIPHfJeB3qC_YCQ=w544-h544-l90-rj",
+    "desc": "이 앨범은 사랑에 대한 깨달음을 이야기하고 있습니다. 나 자신에 대한 사랑, 타인과의 관계 속에서 사랑, 그리고 그 두 가지를 깨달은 후 얻은 궁극적인 사랑! 그러니까 ‘마지막에 남은 것’은 사랑입니다. 나 자신에 대한 그리고 타인과의 관계 속에서 설명할 수 없을만큼 복잡한 감정들 속에서 괴로울 때, 그럼에도 불구하고 가장 중요한 것 하나만 마음에 새기면 어느새 단순해지고 기분이 나아져 있었습니다. 미움, 그리움, 괴로움, 불안 같은 부정적인 것들을 모두 제쳐두고 가장 중요하게, 마지막에 남아 있는 것을 보니 그것이 사랑이었다는 이야기입니다.",
+    "link": "https://music.youtube.com/playlist?list=OLAK5uy_kEVIB65xg6T3gUo5fCHSCMDC8tvq7l3FE"
+},
+{
+    "title": "Korn - Follow The Leader",
+    "img": "https://lh3.googleusercontent.com/4SvxoyJf8oscWyfPEN3sQsiTpzcQ_4ko4x0ywY_-p4GNkLql4RQJh0GfF85Bumm8O2ODKfhsS2tya1Px0Q=w544-h544-l90-rj",
+    "desc": "Follow the Leader (stylized as FOLLOW The LEADEЯ) is the third studio album by American nu metal band Korn. The album was released on August 18, 1998, through Immortal/Epic. This was their first album not produced by Ross Robinson. Instead, it was produced by Steve Thompson and Toby Wright.",
+    "link": "https://music.youtube.com/playlist?list=OLAK5uy_l9DBNhKxExv0Xd2CrzEY0vmUupeVjzRVw"
+},
+{
+    "title": "Pavement - Crooked Rain, Crooked Rain",
+    "img": "https://lh3.googleusercontent.com/160b6SHUHXWexaTg6zvWlLgbII-cFT-Lm1BfIcn_z01K3XTOKexkAe5ntpgwvFiD2GUDqEmVlnJvlgtl=w544-h544-l90-rj",
+    "desc": "몇번의 멤버 교체 후 밴드는 1994년 정규 두번째 앨범인 본작을 발표한다. 1집의 엄청난 찬사로 슬래커인 스티븐 말크머스는 그만하고 싶다고 너스레를 떨기도 하였으나, 양손 가득 냉소 한바가지 담긴 아이러니와 브릿팝이 연상되는 수려한 멜로디 전개로 올뮤직, 피치포크 미디어 등에서 만점을 받는 명반을 만들어 낸다.(롤링 스톤지에서는 별 4개의 평가를 받았다.) 개별 트랙에 있어서도 피치포크 90년대 최고의 노래에 Gold Soundz가 1위로 선정되는 등 좋은 평가를 받는다.",
+    "link": "https://music.youtube.com/playlist?list=OLAK5uy_mdyuZeyu-1Te2D4CMLNLucul7v7HoWISA"
+}]
+
+function addGuestBook() {
+    let name = document.querySelector("#name").value
+    if (name === "")
+        return
+    let checkedMovie = [1, 2, 3, 4, 5].map((value) => {
+        let movie = document.querySelector(`#movie${value}`)
+        if (movie.checked)
+            return movie.nextElementSibling.textContent.trim()
+        else
+            return ""
+    }).filter(Boolean)
+    let checkedAlbum = [1, 2, 3, 4, 5].map((value) => {
+        let album = document.querySelector(`#album${value}`)
+        if (album.checked)
+            return album.nextElementSibling.textContent.trim()
+        else
+            return ""
+    }).filter(Boolean)
+    let checked = checkedMovie.concat(checkedAlbum)
+    let guestBook =
+        `<hr>
+        <h2>${name}</h2>
+        <p style="width: 300px;">${checked.join(", ")}</p>`
+    let div = document.createElement('div')
+    div.innerHTML = guestBook
+    document.querySelector("#guestbook-div").append(div)
+    alert(`${name}님, 저와 ${checked.length}개의 취향이 같으시네요!`)
+    resetGuestBook()
+}
+
+function resetGuestBook() {
+    [1, 2, 3, 4, 5].map((value) => {
+        let movie = document.querySelector(`#movie${value}`)
+        let album = document.querySelector(`#album${value}`)
+        movie.checked = false
+        album.checked = false
+        document.querySelector("#name").value = ""
+    })
+}
+
+function init() {
+    initTable(movies, "poster", "#h2 > table:nth-child(2) > tbody")
+    initTable(albums, "cover", "#h2 > table:nth-child(4) > tbody")
+    initGuestBook(movies, "movie", "#guestbook-movie", null)
+    initGuestBook(albums, "album", "#guestbook-album", "#guestbook-album > button")
+}
+
+function initTable(list, type, selector) {
+    list.forEach((element, index) => {
+        let column =
+            `<td>${index + 1}</td>
+            <td><img class="${type}" src="${element.img}" /></td>
+            <td>${element.title}</td>
+            <td>${element.desc}</td>
+            <td><a target="_blank" href="${element.link}">바로가기</a></td>`
+        let tr = document.createElement('tr')
+        tr.innerHTML = column
+        document.querySelector(selector).append(tr)
+    });
+}
+
+function initGuestBook(list, type, selector, before) {
+    list.forEach((element, index) => {
+        let check =
+            `<input class="form-check-input" type="checkbox" value="" id="${type}${index + 1}">
+            <label style="width: 300px;" class="form-check-label" for="${type}${index + 1}">
+                ${element.title}
+            </label>`
+        let div = document.createElement('div')
+        div.className = "form-check"
+        div.innerHTML = check
+        document.querySelector(selector).insertBefore(div, document.querySelector(before))
+    })
+}
+
+window.onload = init
